5a0ee9864c94dc7145459fc3a106ecc5bd29cf89
Agung RB <agungrbudiman@gmail.com>
2022-07-21 23:54:55 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+function capFirst(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+function getComputerChoice() {
+    generatedNum = Math.floor(Math.random() * 3)
+    switch(generatedNum) {
+        case 0: return 'rock'
+        case 1: return 'paper'
+        case 2: return 'scissors'
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    if (playerSelection == computerSelection) {
+        return 'draw'
+    }
+    if ((playerSelection == 'rock') && (computerSelection == 'scissors')) {
+        return 'win'
+    }
+    if ((playerSelection == 'paper') && (computerSelection == 'rock')) {
+        return 'win'
+    }
+    if ((playerSelection == 'scissors') && (computerSelection == 'paper')) {
+        return 'win'
+    }
+    return 'lose'
+}
+
+function game() {
+    let playerScore = 0
+    let computerScore = 0
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Enter your choice')
+        const computerSelection = getComputerChoice()
+        let result = playRound(playerSelection, computerSelection)
+        result == 'win' ? playerScore++ : null
+        result == 'lose' ? computerScore++ : null
+        if (result == 'win') {
+            console.log(`Round-${i+1} | You Win!, ${playerSelection} beats ${computerSelection}`)
+        }
+        else if (result == 'lose') {
+            console.log(`Round-${i+1} | You Lose!, ${computerSelection} beats ${playerSelection}`)
+        }
+        else {
+            console.log(`Round-${i+1} | Draw!, ${playerSelection} vs ${computerSelection}`)
+        }
+    }
+    if (playerScore == computerScore) {
+        console.log('Result: Draw!')
+    }
+    else if (playerScore > computerScore) {
+        console.log('Result: You are the winner!')
+    }
+    else {
+        console.log('Result: Computer is the winner!')
+    }
+}
+
+game()
